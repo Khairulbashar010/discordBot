fdb24eb811125f11cb72f84936a4dc17b31eec8d
Khairul <khairul.bashar.rpsu@gmail.com>
2022-02-21 02:27:41 +0600

    discord bot completed

--- bot.js
+++ bot.js
@@ -1,36 +1,65 @@
 require('dotenv').config();
 const lib = require('./lib');
-const { Client, Intents, RichEmbed } = require('discord.js');
-const PREFIX = "!join-";
+const { Client, Intents } = require('discord.js');
+const PREFIX = process.env.PREFIX;
+const BOTCHANNEL = process.env.BOTCHANNEL;
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, "GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES"],
+    partials: ['CHANNEL']
+});
+
 client.on('ready', () => {
     console.log(`[${client.user.tag}] has loged in`)
 })
 
-client.on('messageCreate', async(message) => {
-    if(message.author.bot) return;
-    if(message.content.startsWith(PREFIX)){
+client.on('messageCreate', async (message) => {
+    if (message.author.bot) return;                                             // skip bot messages
+    if(message.guildId){                                                        // message from a channel
+        var botChannel = message.channel.guild.channels.cache.find((channel) => channel.name.toLowerCase() === BOTCHANNEL);    // general channel
+    }
+    // invoke bot with '!join-' in defined channel
+    if (message.content.startsWith(PREFIX)) {
+        if(!message.guildId || message.channelId != botChannel.id) return;      // DM or not the bot channel
         var clan = message.content.substring(PREFIX.length).split(" ")[0];
-        if(clan.length > 0){
-            var validateCommand = await lib.commandValidation(clan);
+        if (clan.length > 0) {
+            var validateCommand = await lib.commandValidation(clan);            //validate the given command
         }
-        message.reply(validateCommand.message);
-        if(validateCommand.success){
-            let firstMessage = await message.author.send("Please provide your email id");
-            let filter = () => true; // you don't need it, since it's a DM.
-            let collected = await firstMessage.channel.awaitMessages(filter, {
-                maxMatches: 1, // you only need one message
-                time: 10000 // the time you want it to run for
-                }).catch(console.log);
-
-            if (collected && collected.size > 0) {
-                let email = collected.first().content.split(' ')[0]; // grab the email
-                collected.forEach(msg => msg.delete()); // delete every collected message (and so the email)
-                await firstMessage.edit("Email saved!"); // edit the first message you sent
-            } else await firstMessage.edit("Command timed out :("); // no message has been received
-
-            firstMessage.delete(30000); // delete it after 30 seconds
+        message.channel.send(`${message.author}, ${validateCommand.message}`);  // response after validation
+        if (validateCommand.success) {
+            // Direct Messages
+            const firstMessage = await message.author.send("Please provide your email id");
+            const filter = () => true;      // no filter needed
+            const collector = firstMessage.channel.createMessageCollector({
+                filter,
+                maxProcessed: 1,            // need one message
+                time: 60 * 5 * 1000,        // the time to wait for
+                errors: ['time']
+            })
+            collector.on("collect", async (dmMessage) => {        // on receiving a message
+                let userObj = {
+                    Email: dmMessage.content,
+                    commandData: validateCommand.commandData
+                }
+                var validateUser = await lib.userValidation(userObj); // validate given email
+                if (validateUser.success) {
+                    const guild = client.guilds.cache.get(message.guildId);     // as no guildId present in DM message
+                    const role = guild.roles.cache.get(userObj.commandData.RoleId);
+                    var isMember = guild.members.cache.get(message.author.id);  // to check if user still in server
+                    if (isMember) {
+                        if (isMember.roles.cache.some(roles => roles.id === role.id)) { // check if already has role
+                            await isMember.send("Already in the clan.");
+                        } else {
+                            await isMember.roles.add(role);     // add the role
+                            await client.channels.cache.get(userObj.commandData.ChannelId).send(`Welcome to the clan, ${isMember}`);        // welcome message
+                        }
+                    }
+                } else {
+                    await message.author.send(validateUser.message);    // couldn't validate email
+                }
+                collector.stop("collected");
+            })
+            collector.on('end', async (collected, reason) => reason == 'time' ? await message.author.send("Timeout :(\nPlease retry"): console.log(reason));        // timeout message
         }
 
     }
--- lib.js
+++ lib.js
@@ -1,39 +1,76 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const creds = require(process.env.SERVICEACCOUNTKEY); // location of the apiKey from service account
-const doc = new GoogleSpreadsheet(process.env.SHEETID); // Initialize the sheet
+
 
 async function commandValidation(clan) {
+	const doc = new GoogleSpreadsheet(process.env.SHEETID); // Initialize Sheet
 	await doc.useServiceAccountAuth(creds); // Initialize Auth
-	await doc.loadInfo();  // loads document properties and worksheets
-	const sheet = doc.sheetsByTitle["Sheet2"];
-	let rows = await sheet.getRows();
+	await doc.loadInfo();
+	const initialSheet = doc.sheetsByIndex[1]; // searches the second sheet for commands
+	let rows = await initialSheet.getRows();
 
 	let targetRow = null;
+	// default response
 	let response  = {
-		message: "Oops! Clan not found :(",
+		message: "Clan not found :(",
 		success: false,
+		commandData: null
 	}
 
-	for (let i = 0; i < rows.length; i++) {
+	for (let i = 0; i < rows.length; i++) { // iterate over rows
 		const row = rows[i];
-		if (row.Clan == clan) {
+		if (row.Clan == clan) { // clan name exists
 			targetRow = row;
 			break;
 		}
 	}
 	if(targetRow) {
-		if(targetRow['Enabled/Disabled'] === "TRUE") {
+		if(targetRow['Enabled/Disabled'] == "TRUE") {
+			// if clan exists and commad is active, update response
 			response.message = "Please check your DM to verify your id";
 			response.success = true;
-			return response;
+			response.commandData = targetRow;
 		} else {
+			// if clan exists and commad is inactive
 			response.message = "Sorry, admissions have been closed";
-			return response;
 		}
 	}
 	return response;
 }
 
+
+async function userValidation(userObj){
+	const memberDoc = new GoogleSpreadsheet(process.env.SHEETID);
+	await memberDoc.useServiceAccountAuth(creds); // Initialize Auth
+	await memberDoc.loadInfo();
+	const memberSheet = memberDoc.sheetsById[0];	// searches the first sheet for members
+	var memberRows = await memberSheet.getRows();
+	// default response
+	let response  = {
+		message: "Sorry this email is not registered with us! Please enter a valid email which you registered! Type the command again in bot channel to start verification process again.",
+		success: false,
+	}
+	for (let i = 0; i < memberRows.length; i++) {
+		const row = memberRows[i];
+		if (row.Email === userObj.Email) {		// iterate over rows
+			if(row.ChannelId == userObj.commandData.ChannelId && userObj.commandData.ChannelId) {	// if data already exists
+				response.message = "Already in the clan.";
+				break;
+			}
+			// asign new data
+			memberRows[i].ChannelId = userObj.commandData.ChannelId;
+			memberRows[i].RoleId = userObj.commandData.RoleId;
+			await memberRows[i].save();
+			response.message = "Successfully verified.";
+			response.success = true;
+			break;
+		}
+	}
+	return response;
+}
+
+
 module.exports = {
-	commandValidation
+	commandValidation,
+	userValidation
 };
