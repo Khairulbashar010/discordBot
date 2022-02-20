f3c86a4ae4c78849dabc6131de6c314978325eca
Khairul <khairul.bashar.rpsu@gmail.com>
2022-02-20 13:07:33 +0600

    read from sheets

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const lib = require('./lib');
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, RichEmbed } = require('discord.js');
+const PREFIX = "!join-";
 
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 client.on('ready', () => {
@@ -8,8 +9,31 @@ client.on('ready', () => {
 })
 
 client.on('messageCreate', async(message) => {
-    var processedMessage = await lib.checkMessage(message);
-    console.log(processedMessage)
+    if(message.author.bot) return;
+    if(message.content.startsWith(PREFIX)){
+        var clan = message.content.substring(PREFIX.length).split(" ")[0];
+        if(clan.length > 0){
+            var validateCommand = await lib.commandValidation(clan);
+        }
+        message.reply(validateCommand.message);
+        if(validateCommand.success){
+            let firstMessage = await message.author.send("Please provide your email id");
+            let filter = () => true; // you don't need it, since it's a DM.
+            let collected = await firstMessage.channel.awaitMessages(filter, {
+                maxMatches: 1, // you only need one message
+                time: 10000 // the time you want it to run for
+                }).catch(console.log);
+
+            if (collected && collected.size > 0) {
+                let email = collected.first().content.split(' ')[0]; // grab the email
+                collected.forEach(msg => msg.delete()); // delete every collected message (and so the email)
+                await firstMessage.edit("Email saved!"); // edit the first message you sent
+            } else await firstMessage.edit("Command timed out :("); // no message has been received
+
+            firstMessage.delete(30000); // delete it after 30 seconds
+        }
+
+    }
 })
 
 client.login(process.env.BOT_TOKEN);
--- lib.js
+++ lib.js
@@ -1,9 +1,39 @@
-const prefix = "!";
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const creds = require(process.env.SERVICEACCOUNTKEY); // location of the apiKey from service account
+const doc = new GoogleSpreadsheet(process.env.SHEETID); // Initialize the sheet
 
+async function commandValidation(clan) {
+	await doc.useServiceAccountAuth(creds); // Initialize Auth
+	await doc.loadInfo();  // loads document properties and worksheets
+	const sheet = doc.sheetsByTitle["Sheet2"];
+	let rows = await sheet.getRows();
 
+	let targetRow = null;
+	let response  = {
+		message: "Oops! Clan not found :(",
+		success: false,
+	}
 
-function checkMessage(message) {
-    return message.content;
+	for (let i = 0; i < rows.length; i++) {
+		const row = rows[i];
+		if (row.Clan == clan) {
+			targetRow = row;
+			break;
+		}
+	}
+	if(targetRow) {
+		if(targetRow['Enabled/Disabled'] === "TRUE") {
+			response.message = "Please check your DM to verify your id";
+			response.success = true;
+			return response;
+		} else {
+			response.message = "Sorry, admissions have been closed";
+			return response;
+		}
+	}
+	return response;
 }
 
-module.exports = { checkMessage };
+module.exports = {
+	commandValidation
+};
